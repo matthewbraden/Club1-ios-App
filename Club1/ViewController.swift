ec6174301f270b488ef9419fa27df5b8391b143a
matthewbraden <bradenm@mcmaster.ca>
2019-08-05 18:40:18 -0400

    Initial Commit

---
+++ Club1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Club1
+//
+//  Created by Matthew Braden on 2019-08-05.
+//  Copyright © 2019 Matthew Braden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
